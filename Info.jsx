1be99e08bf46542d674bee2c4b0e6b46cf80b5a9
Codwing <76616495+Codwing@users.noreply.github.com>
2021-06-28 01:23:22 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Info(){
+  return(
+    <div className="note">
+    <h1>JavaScript and React.js </h1>
+    <p>A basic web dev react Js Bootcamp</p>
+</div>
+  );
+  }
+export default Info;
